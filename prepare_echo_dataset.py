bf84ca1bf39fe9384f860b37a8e76209c909d571
Chris Tralie <chris.tralie@gmail.com>
2024-06-27 00:07:56 -0400

    whoops

--- prepare_echo_dataset.py
+++ prepare_echo_dataset.py
@@ -58,7 +58,7 @@ if __name__ == "__main__":
     parser.add_argument('--alpha', type=float, default=0.4, help='Strength of echo')
     parser.add_argument('--temp_dir', type=str, required=True, help="Path to temporary folder to which to save modified dataset (cleared before and after echoes are created)")
     parser.add_argument('--sr', type=int, default=44100, help="Audio sample rate")
-    parser.add_argument('--n_threads', type=str, default=10, help="Number of threads to use")
+    parser.add_argument('--n_threads', type=int, default=10, help="Number of threads to use")
     opt = parser.parse_args()
 
     prepare_rave(opt.dataset_path, opt.output_path, opt.lag, opt.alpha, opt.temp_dir, opt.sr, opt.n_threads)
