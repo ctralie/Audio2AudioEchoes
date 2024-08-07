f088fb12cbf80d52857c6ad38d2b469810993213
Chris Tralie <chris.tralie@gmail.com>
2024-08-07 12:32:50 -0400

    Dealing with recursive folder structures in chunk_files.py

--- chunk_files.py
+++ chunk_files.py
@@ -22,15 +22,18 @@ if __name__ == "__main__":
     if not os.path.exists(outdir):
         os.mkdir(outdir)
     idx = 0
-    files = glob.glob("{}/*".format(opt.indir))
+    files = glob.glob(opt.indir + os.path.sep + "**", recursive=True)
     for i in tqdm(range(len(files))):
-        x, _ = librosa.load(files[i], sr=sr, mono=False)
-        if len(x.shape) == 1:
-            x = x[None, :]
-        x = np.array(x*32767, dtype=np.int16).T
-        n_splits = max(x.shape[0]//max_len, 1)
-        for k in range(n_splits):
-            xk = x[k*max_len:(k+1)*max_len, :]
-            if xk.shape[0] >= min_len:
-                wavfile.write("{}/{}.wav".format(outdir, idx), sr, xk)
-                idx += 1
+        try:
+            x, _ = librosa.load(files[i], sr=sr, mono=False)
+            if len(x.shape) == 1:
+                x = x[None, :]
+            x = np.array(x*32767, dtype=np.int16).T
+            n_splits = max(x.shape[0]//max_len, 1)
+            for k in range(n_splits):
+                xk = x[k*max_len:(k+1)*max_len, :]
+                if xk.shape[0] >= min_len:
+                    wavfile.write("{}/{}.wav".format(outdir, idx), sr, xk)
+                    idx += 1
+        except:
+            print("Error on", files[i])
