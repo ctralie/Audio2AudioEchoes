c547f3f0d7f7899663852a6dd22073bb212dde09
Chris Tralie <chris.tralie@gmail.com>
2024-10-03 09:59:10 -0400

    Adding the option to only tag a percentage of a dataset

--- prepare_echo_dataset.py
+++ prepare_echo_dataset.py
@@ -19,13 +19,17 @@ def add_echo(params):
     lags = [int(l) for l in lags.split(",")]
     tic = time.time()
     x = load_audio_fast_wav(filename_in, sr)
-    x = echo_hide_constant(x, lags, [alpha]*len(lags))
+    if lags[0] > -1:
+        print("Tagging", filename_in)
+        x = echo_hide_constant(x, lags, [alpha]*len(lags))
+    else:
+        print("Not tagging", filename_in)
     x = np.array(x*32767, dtype=np.int16)
     wavfile.write(filename_out, sr, x)
-    print("{}, {}, Elapsed {}\n\n".format(filename_in, filename_out, time.time()-tic))
+    #print("{}, {}, Elapsed {}\n\n".format(filename_in, filename_out, time.time()-tic))
 
 
-def prepare_dataset(dataset_path, output_path, lag, alpha, temp_dir, sr=44100, n_threads=10, use_rave=True):
+def prepare_dataset(dataset_path, output_path, lag, alpha, temp_dir, sr=44100, n_threads=10, perc=1, use_rave=True):
     ## Step 1: Cleanup temp directory
     for f in glob.glob("{}/*".format(temp_dir)):
         os.remove(f)
@@ -34,8 +38,16 @@ def prepare_dataset(dataset_path, output_path, lag, alpha, temp_dir, sr=44100, n
     filenames_in = walk_dir(dataset_path)
     N = len(filenames_in)
     filenames_out = ["{}/{}.wav".format(temp_dir, i) for i in range(N)]
+    lags = [lag]*N
+    if perc < 1:
+        # Only tagging part of the dataset
+        lags = ["-1"]*N
+        np.random.seed(int(100000*perc))
+        for idx in np.random.permutation(N)[0:int(N*perc)]:
+            lags[idx] = lag
+
     with Pool(n_threads) as p:
-        p.map(add_echo, zip(filenames_in, filenames_out, [sr]*N, [lag]*N, [alpha]*N))
+        p.map(add_echo, zip(filenames_in, filenames_out, [sr]*N, lags, [alpha]*N))
 
     if not os.path.exists(output_path):
         os.mkdir(output_path)
@@ -69,9 +81,10 @@ if __name__ == "__main__":
     parser.add_argument('--sr', type=int, default=44100, help="Audio sample rate")
     parser.add_argument('--n_threads', type=int, default=10, help="Number of threads to use")
     parser.add_argument("--use_rave", type=int, default=1, help="Whether to use rave.")
+    parser.add_argument("--perc", type=float, default=1, help="Proportion of dataset to tag (default 1 for all)")
     opt = parser.parse_args()
 
-    prepare_dataset(opt.dataset_path, opt.output_path, opt.lags, opt.alpha, opt.temp_dir, opt.sr, opt.n_threads, opt.use_rave==1)
+    prepare_dataset(opt.dataset_path, opt.output_path, opt.lags, opt.alpha, opt.temp_dir, opt.sr, opt.n_threads, opt.perc, opt.use_rave==1)
     
 
 
